3b902b472b43243858055146083865b2329b931b
unknown <levi.millikin@gmail.com>
2023-01-13 19:49:10 -0800

    new chgange

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 
 
+
 // 👇 Start handling routes here
 // Contrary to the views version, all routes are controlled from the routes/index.js
 const allRoutes = require("./routes/index.routes");
