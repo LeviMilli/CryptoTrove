15964b52dde8604c1b0761c1b5a281d1f2e2e8c9
unknown <levi.millikin@gmail.com>
2023-01-23 03:28:06 -0800

    apis set for server

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(session({
 	  maxAge: 1000 * 24* 60 * 60 // your cookie will be cleared after these seconds
 	},
 	store: MongoStore.create({
-	  mongoUrl: process.env.MONGODB_URI || "mongodb://localhost/foodSnitch",
+	  mongoUrl: process.env.MONGODB_URI || "mongodb://localhost/CryptoTrove",
 	  // Time to Live for sessions in DB. After that time it will delete it!
 	  ttl: 24* 60 * 60 // your session will be cleared after these seconds
 	})
